fa7fac095b26b1eaf078bcf88a63b534a4f45918
cse-f <verify.moneymind@gmail.com>
2025-10-29 21:03:47 +0530

    Create first.js

---
+++ first.js
@@ -0,0 +1,62 @@
+wget --no-check-certificate https://archive.apache.org/dist/kafka/0.8.2.2/kafka_2.10-0.8.2.2.tgz
+tar -xzf kafka_2.10-0.8.2.2.tgz
+sudo mv kafka_2.10-0.8.2.2 /usr/lib/kafka
+cd /usr/lib/kafka
+
+to run zookeepr start port at 2181
+bin/zookeeper-server-start.sh config/zookeeper.properties
+to know whether it is running or not
+sudo netstat -tulnp | grep 2181
+
+to run kafka port at 9092
+bin/kafka-server-start.sh config/server.properties
+
+bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic test-topic
+to check all topoics
+bin/kafka-topics.sh --list --zookeeper localhost:2181
+
+producer
+bin/kafka-console-producer.sh --broker-list localhost:9092 --topic test-topic
+consumer
+bin/kafka-console-consumer.sh --zookeeper localhost:2181 --topic test-topic --from-beginning
+bin/kafka-console-consumer.sh --zookeeper localhost:2181 --topic test-topic --from-beginning | nc -lk 9999
+
+in home/cloudera
+nano spark_socket_consumer.py
+
+from pyspark import SparkConf, SparkContext
+from pyspark.streaming import StreamingContext
+
+conf = SparkConf().setAppName("SocketKafkaForwardConsumer").setMaster("local[2]")
+sc = SparkContext(conf=conf)
+ssc = StreamingContext(sc, 2)  # 2-second batch interval
+
+lines = ssc.socketTextStream("localhost", 9999)
+
+def process(rdd):
+    count = rdd.count()
+    if count > 0:
+        print(f"Received {count} records in this batch")
+        for i, record in enumerate(rdd.take(10), start=1):
+            print(f"[{i}] {record}")
+    else:
+        print("No records in this batch")
+
+lines.foreachRDD(process)
+
+ssc.start()
+ssc.awaitTermination()
+
+ctrl+o enter ctrl+x
+
+now find spark
+it will be in usr/lib
+cd /usr/lib
+cd spark
+ls bin ----you should able to see spark-submit
+
+bin/spark-submit --master local[2] /home/cloudera/spark_socket_consumer.py
+
+
+
+
